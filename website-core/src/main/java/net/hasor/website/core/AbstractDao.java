6b65fa545ba6731c7201f17ac14d4341e46c2530
zyc <zyc@hasor.net>
2018-03-30 11:26:22 +0800

    1.更新到最新版本的 Hasor。

--- website-core/src/main/java/net/hasor/website/core/AbstractDao.java
+++ website-core/src/main/java/net/hasor/website/core/AbstractDao.java
@@ -15,7 +15,6 @@
  */
 package net.hasor.website.core;
 import net.hasor.core.Inject;
-import net.hasor.db.jdbc.JdbcOperations;
 import net.hasor.plugins.mybatis3.SqlExecutorOperations;
 import net.hasor.website.domain.beans.AppConstant;
 import org.slf4j.Logger;
@@ -27,16 +26,10 @@ import org.slf4j.LoggerFactory;
 public abstract class AbstractDao<T> {
     protected Logger logger = LoggerFactory.getLogger(getClass());
     @Inject(AppConstant.DB_MYSQL)
-    private JdbcOperations        jdbcTemplate;
-    @Inject(AppConstant.DB_MYSQL)
     private SqlExecutorOperations executorTemplate;
     //
     protected SqlExecutorOperations getSqlExecutor() {
         return this.executorTemplate;
-    }
-    //
-    protected JdbcOperations getJdbcTemplate() {
-        return this.jdbcTemplate;
     }
     //
 }
